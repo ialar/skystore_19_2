9b4fa200b9f7473a307f3c9efc14d4a45073c7a2
Igor Larionov <jedialar@gmail.com>
2024-03-17 19:24:13 +0300

    edit Product in models.py and cancel 3rd migration

--- catalog/models.py
+++ catalog/models.py
@@ -23,6 +23,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Цена', **NULLABLE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    # manufactured_at = models.DateField(verbose_name='Дата производства продукта', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} ({self.category}) - {self.price} руб.'
